3aba1588a5a543f2326de3c4a08f822eca88e865
dennesshen <dennesshen@gmail.com>
2022-02-16 02:34:43 +0800

    Ｍac 20220216

---
+++ SpringBoot-Portforlio-20211018/src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/page")
+public class PageController {
+	
+	@RequestMapping("/classify")
+	public String getClassifyPage() {
+		return"classify.html";
+	}
+	
+	@RequestMapping("/home")
+	public String getHomePage() {
+		return"home.html";
+	}
+
+}
